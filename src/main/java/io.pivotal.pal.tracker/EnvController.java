670c583024f7885ec15e007c83040fdba3fa6d9c
Tim <tbatty2@ford.com>
2018-10-02 08:46:04 -0600

    changed manifest to deploy through Travis CI

--- src/main/java/io.pivotal.pal.tracker/EnvController.java
+++ src/main/java/io.pivotal.pal.tracker/EnvController.java
@@ -16,7 +16,10 @@ public class EnvController {
     private final String cfInstanceIndex;
     private final String cfInstanceAddress;
 
-    public EnvController(@Value("${PORT:NOT SET}") String port, @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit, @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex, @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress) {
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress) {
 
         this.port = port;
         this.memoryLimit = memoryLimit;
